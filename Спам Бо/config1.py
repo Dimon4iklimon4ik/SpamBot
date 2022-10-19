afce501c07d6915809174de3fa8fc1147030dea7
Dimon4iklimon4ik <107960715+Dimon4iklimon4ik@users.noreply.github.com>
2022-10-19 23:41:19 +0300

    Add files via upload

---
+++ Спам Бо/config1.py
@@ -0,0 +1,10 @@
+
+#BOT:
+TOKEN = "5738229728:AAG2ZsVGnsmrp1Zcbhg3o38sNT1KH2aF3YY" # ТОКЕН БОТА
+ADMIN = 5004321454 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # 
+API_ID = 17726637
+API_HASH = "1d9ed985484714f7202adf315950f2e0"
